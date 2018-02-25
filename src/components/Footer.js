eb5f6997b66b3a22e93a76cf27db19078504808d
Sasha Kulynych <sashakulinich97@gmail.com>
2018-02-25 17:50:25 +0200

    updateAuthReg

---
+++ src/components/Footer.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import './styles/footer.css';
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <div className="container main footer">
+                <div className="row ml-0 mr-0">
+                    <div className="col-3 footerBlockLeft d-flex justify-content-center align-items-center">
+                        <div className="footerLogoTextLeft">Клімат</div>
+                        <img className="footerLogoImage" src={require("./images/logo.png")} alt="Logo"/>
+                        <div className="footerLogoTextRight">Комплект</div>
+                    </div>
+                    <div className="col-6  d-flex justify-content-center footerCenter">
+                        <div className="col d-flex justify-content-start">
+                            <ul className="footerList">
+                                <li>Про нас</li>
+                                <li>Обладнання</li>
+                                <li>Референс</li>
+                                <li>Контакти</li>
+                            </ul>
+                        </div>
+                        <div className="col">
+                            <ul className="contacts">
+                                <li>Контакти</li>
+                                <li><i class="fas fa-envelope contactIcons"></i>email@gmail.com</li>
+                                <li><i class="fas fa-phone-volume contactIcons"></i>+38 099 001 00 00</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="col-3 d-flex align-items-center justify-content-center">
+                        <div className="delivery">
+                            <img src={require("./images/truck.png")} style={{width:150}} alt=""/>
+                            <div className="deliveryText">Доставка 24 години</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,133 @@
 import React, { Component } from 'react';
 import './styles/header.css';
-
+import Modal from "react-responsive-modal";
 export default class Header extends Component {
+    state = {
+        openAuth: false,
+        openReg:false,
+        personType:0
+    };
+    personType(index){
+       this.setState({personType:index})
+    }
+    openRegonOpenModal = () => {
+        this.setState({ openReg: true });
+    };
+
+    openRegonCloseModal = () => {
+        this.setState({ openReg: false });
+    };
+
+    openAuthonOpenModal = () => {
+        this.setState({ openAuth: true });
+    };
+
+    openAuthonCloseModal = () => {
+        this.setState({ openAuth: false });
+    };
     render() {
+        const { openAuth,openReg } = this.state;
         return (
             <div className="container main">
+                <Modal showCloseIcon={false} open={openReg} onClose={this.openRegonCloseModal} little>
+                    <div className="authorizationHeader d-flex align-items-center justify-content-center">
+                        <span>РЕЄСТРАЦІЯ</span>
+                    </div>
+                    <div className="authorization">
+                        <div className="authGroup">
+                            <div className="authInputGroup row d-flex align-items-center justify-content-center">
+                                <div className="authInputIcon d-flex align-items-center justify-content-center">
+                                    <i class="fas fa-user"></i>
+                                </div>
+                                <input type="text" placeholder="ПІБ"/>
+                            </div>
+                            <div className="row d-flex justify-content-between align-items-center paddingHorizontal15">
+                                <div className="typeButtons"
+                                     onClick={()=>this.personType(0)}
+                                     style={this.state.personType===0?{backgroundColor:'#fed328'}:null}
+                                >Компанія</div>
+                                <div className="typeButtons"
+                                     onClick={()=>this.personType(1)}
+                                     style={this.state.personType===1?{backgroundColor:'#fed328'}:null}
+                                >Приватна особа</div>
+                            </div>
+                            <div className="authInputGroup row d-flex align-items-center justify-content-center">
+                                <div className="authInputIcon d-flex align-items-center justify-content-center">
+                                    <i class="fas fa-at"></i>
+                                </div>
+                                <input type="email" placeholder="email"/>
+                            </div>
+                            <div className="authInputGroup row d-flex align-items-center justify-content-center">
+                                <div className="authInputIcon d-flex align-items-center justify-content-center">
+                                    <i class="fas fa-phone"></i>
+                                </div>
+                                <input id="phone" type="text" placeholder="номер телефону"/>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col">
+                                <div className="buttonEnter">Зареєструватися</div>
+                            </div>
+                            <div className="col">
+                                <div className="d-flex justify-content-end d-flex align-items-center"
+                                     onClick={()=>{
+                                         this.openRegonCloseModal()
+                                         this.openAuthonOpenModal()
+                                     }}
+                                >
+                                    <span className="rememberMe">Назад</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </Modal>
+                <Modal showCloseIcon={false} open={openAuth} onClose={this.openAuthonCloseModal} little>
+                    <div className="authorizationHeader d-flex align-items-center justify-content-center">
+                        <span>ВХІД</span>
+                    </div>
+                    <div className="authorization">
+                        <div className="authGroup">
+                            <div className="authInputGroup row d-flex align-items-center justify-content-center">
+                                <div className="authInputIcon d-flex align-items-center justify-content-center">
+                                    <i class="fas fa-user"></i>
+                                </div>
+                                <input type="text" placeholder="username"/>
+                            </div>
+                            <div className="authInputGroup row d-flex align-items-center justify-content-center">
+                                <div className="authInputIcon d-flex align-items-center justify-content-center">
+                                    <i class="fas fa-unlock-alt"></i>
+                                </div>
+                                <input type="password" placeholder="password"/>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col">
+                                <div className="buttonEnter">Увійти</div>
+                            </div>
+                            <div className="col">
+                                <div className="d-flex align-items-center justify-content-end">
+                                    <input type="checkbox"/>
+                                    <label className="rememberMe">Запам'ятати мене</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row reg_forgetPassword">
+                            <div className="col">
+                                <div className="d-flex justify-content-start">
+                                    <span className="registrationView" onClick={()=>{
+                                        this.openAuthonCloseModal()
+                                        this.openRegonOpenModal()
+                                    }}>Зареєструватися</span>
+                                </div>
+                            </div>
+                            <div className="col">
+                                <div className="d-flex justify-content-end">
+                                    <span className="forgerPassword">Забув пароль</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </Modal>
                 <div className="row header d-flex align-items-center">
                     <div className="col">
                         <div className="row">
@@ -23,10 +146,10 @@ export default class Header extends Component {
                         <div className="row d-flex align-items-center">
                             <div className="text phone">+38 099 001 01 01</div>
                             <div className="text email">email_adress@gmail.com</div>
-                            <div className="circle">
+                            <div className="circle" onClick={this.openAuthonOpenModal}>
                                 <i class="fas fa-user"></i>
                             </div>
-                            <div className="text enter">Вхід</div>
+                            <div className="text enter" onClick={this.openAuthonOpenModal}>Вхід</div>
                         </div>
                     </div>
                 </div>
