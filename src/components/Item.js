642ed00510022161556219d083c34af509944083
sergey-grishko <megusta970@gmail.com>
2018-04-12 17:21:14 +0300

    v.xz

--- src/components/Item.js
+++ src/components/Item.js
@@ -6,6 +6,7 @@ import { css, StyleSheet } from 'aphrodite'
 
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router'
+import {AreaChart, Area, XAxis, YAxis, Tooltip, CartesianGrid} from 'recharts';
 
 import * as API from '../actions/api'
 
@@ -52,9 +53,9 @@ class Item extends Component {
                 <Header/>
                 <div className="item">
                     <div className="row itemContainer">
-                        <div className="col">
+                        <div className="col-sm-12 col-lg-6">
                             <img className="itemImage" src={require('./images/cooler.png')} alt=""/>
-                            <div className="d-flex justify-content-between">
+                            <div className="d-flex justify-content-between row">
                                 <div className="leftPartCircle"></div>
                                 <div className="leftPartCircle"></div>
                                 <div className="leftPartCircle"></div>
@@ -74,13 +75,13 @@ class Item extends Component {
                                 {this.state.product.desc}
                             </div>
                             <div className="row">
-                                <div className="rightButtonStyle">
+                                <div className="rightButtonStyle col-sm-12">
                                     <span>Опис</span>
                                 </div>
-                                <div className="rightButtonStyle">
+                                <div className="rightButtonStyle col-sm-12">
                                     <span>Технічні дані</span>
                                 </div>
-                                <div className="rightButtonStyle">
+                                <div className="rightButtonStyle col-sm-12">
                                     <span>Характеристики</span>
                                 </div>
                             </div>
@@ -101,10 +102,48 @@ class Item extends Component {
                         </div>
                     </div>
 
+                    <div className="graf_1">
+                        <AreaChart  className="col-12 row"  width={750} height={350} data={this.state.data}>
+                            <defs >
+                                <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                                    <stop offset="5%" stopColor="rgb(2, 74, 229)" stopOpacity={0.8}/>
+                                    <stop offset="95%" stopColor="rgb(2, 74, 229)" stopOpacity={0.1}/>
+                                </linearGradient>
+                                <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                                    <stop offset="5%" stopColor="rgb(214, 221, 6)" stopOpacity={0.8}/>
+                                    <stop offset="95%" stopColor="rgb(214, 221, 6)" stopOpacity={0.1}/>
+                                </linearGradient>
+                            </defs>
+                            <XAxis dataKey="name" stroke="#212121"/>
+                            <YAxis stroke="#212121"/>
+                            <Tooltip/>
+                            <Area type="monotone" dataKey="total" stroke="#8884d8" fill="url(#colorUv)"/>
+                            <Area type="monotone" dataKey="paid" stroke="#82ca9d" fill="url(#colorPv)"/>
+                        </AreaChart>
+                    </div>
+
+                    <div className="graf_2">
+                        <AreaChart className="col-12 row"  width={330} height={350} data={this.state.data}>
+                            <defs >
+                                <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                                    <stop offset="5%" stopColor="rgb(2, 74, 229)" stopOpacity={0.8}/>
+                                    <stop offset="95%" stopColor="rgb(2, 74, 229)" stopOpacity={0.1}/>
+                                </linearGradient>
+                                <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                                    <stop offset="5%" stopColor="rgb(214, 221, 6)" stopOpacity={0.8}/>
+                                    <stop offset="95%" stopColor="rgb(214, 221, 6)" stopOpacity={0.1}/>
+                                </linearGradient>
+                            </defs>
+                            <XAxis dataKey="name" stroke="#212121"/>
+                            <YAxis stroke="#212121"/>
+                            <Tooltip/>
+                            <Area type="monotone" dataKey="total" stroke="#8884d8" fill="url(#colorUv)"/>
+                            <Area type="monotone" dataKey="paid" stroke="#82ca9d" fill="url(#colorPv)"/>
+                        </AreaChart>
+                    </div>
                 </div>
                 <Footer/>
             </div>
         )
     }}
-
 export default connect(state => ({state:state}))(withRouter(Item))
